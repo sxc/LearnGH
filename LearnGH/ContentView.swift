7261dec96465a064c13dc40bb60c8e0b6f7b2c3e
sxc <qiuyequ@icloud.com>
2022-06-21 11:51:01 +0800

    Add image

--- LearnGH/ContentView.swift
+++ LearnGH/ContentView.swift
@@ -9,8 +9,11 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Image(systemName: "figure.wave")
+            Text("Hello, world!")
+                .padding()
+        }
     }
 }
 
