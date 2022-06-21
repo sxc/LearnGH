0aa4f7cd4ea81498a5c6dab974d7571a6a0237f7
sxc <qiuyequ@icloud.com>
2022-06-21 11:30:23 +0800

    Initial Commit

---
+++ LearnGH/LearnGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnGHApp.swift
+//  LearnGH
+//
+//  Created by Xiaochun Shen on 2022/6/21.
+//
+
+import SwiftUI
+
+@main
+struct LearnGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
